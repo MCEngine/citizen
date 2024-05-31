94e0db313d4d2527b83c77e90c66014ca2c0f2df
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-31 05:59:21 +0000

    set up

---
+++ spigot/src/main/java/com/github/mcengine/Citizen.java
@@ -0,0 +1,11 @@
+package com.github.mcengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Citizen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+    @Override
+    public void onDisable() {}
+}
